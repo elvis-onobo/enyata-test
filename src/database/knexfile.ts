270118f233a7f5309f794ec43d43c413ea77129c
Elvis Onobo <elvis.onobo@gmail.com>
2022-10-18 19:48:20 +0100

    Chore: setup docker-compose and database

---
+++ src/database/knexfile.ts
@@ -0,0 +1,79 @@
+import dotenv from 'dotenv'
+import type { Knex } from 'knex'
+// import { attachPaginate } from 'knex-paginate'
+// attachPaginate()
+
+dotenv.config({ path: '../../.env' })
+
+// Update with your config settings.
+const config: { [key: string]: Knex.Config } = {
+ development: {
+  client: 'pg',
+  debug: true,
+  useNullAsDefault: true,
+  connection: {
+   database: process.env.DATABASE_NAME,
+   user: process.env.DATABASE_USER,
+   password: process.env.DATABASE_PASSWORD,
+  },
+  pool: {
+   min: 2,
+   max: 10,
+  },
+  migrations: {
+   tableName: 'knex_migrations',
+   extension: 'ts',
+   directory: './migrations',
+  },
+ },
+
+ test: {
+  client: 'pg',
+  connection: {
+   database: process.env.TEST_DATABASE,
+   user: process.env.DATABASE_USER,
+   password: process.env.DATABASE_PASSWORD,
+  },
+  pool: {
+   min: 2,
+   max: 10,
+  },
+  migrations: {
+   tableName: 'knex_migrations',
+  },
+ },
+
+ staging: {
+  client: 'pg',
+  connection: {
+   database: process.env.DATABASE_NAME,
+   user: process.env.DATABASE_USER,
+   password: process.env.DATABASE_PASSWORD,
+  },
+  pool: {
+   min: 2,
+   max: 10,
+  },
+  migrations: {
+   tableName: 'knex_migrations',
+  },
+ },
+
+ production: {
+  client: 'pg',
+  connection: {
+   database: process.env.DATABASE_NAME,
+   user: process.env.DATABASE_USER,
+   password: process.env.DATABASE_PASSWORD,
+  },
+  pool: {
+   min: 2,
+   max: 10,
+  },
+  migrations: {
+   tableName: 'knex_migrations',
+  },
+ },
+}
+
+export default config
---
+++ src/database/migrations/20221018183725_users.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+ return knex.schema.createTable('users', (table: Knex.TableBuilder) => {
+  table.increments('id', { primaryKey: true })
+  table.uuid('uuid').notNullable().unique()
+  table.string('first_name').notNullable()
+  table.string('last_name').notNullable()
+  table.string('email').notNullable().unique()
+  table.string('password').notNullable()
+  table.timestamps(true, true)
+ })
+}
+
+export async function down(knex: Knex): Promise<void> {
+ return knex.schema.dropTable('users')
+}
