036ee30f27e05db1e0c8d74c6e56f1bfb1161372
Elvis Onobo <elvis.onobo@gmail.com>
2022-10-19 08:34:02 +0100

    Chore: Add auth validation

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express'
 import AuthService from '../services/AuthService'
 import { successHandler } from '../helpers/successHandler'
+import { signupValidator, loginValidator } from '../validation/authValidator'
 
 export default class AuthController {
  public static async signup(req: Request, res: Response): Promise<Response> {
+  await signupValidator.validateAsync(req.body)
   const data = await AuthService.signup(req.body)
   return successHandler('Signup Successful', 200, data)(req, res)
  }
 
  public static async login(req: Request, res: Response): Promise<Response> {
+  await loginValidator.validateAsync(req.body)
   const data = await AuthService.login(req.body)
   return successHandler('Login Successful', 200, data)(req, res)
  }
